efef36e3766af29c04655f478c1ec2e1fad03416
KSniper <zhaoyin214@qq.com>
2019-07-02 15:51:57 +0800

    early_stopping, best_save, reduce_lr_on_plateau

--- configs/config.py
+++ configs/config.py
@@ -1,10 +1,25 @@
 config = dict(train_set_root = "e:/src/jupyter/datasets/CASIA-WebFace",
-              test_set_root = "e:/src/jupyter/datasets/ORL-Face",
               train_batch_size = 64,
               train_epochs = 100,
-              is_rgb = True,
+              val_set_root = "e:/src/jupyter/datasets/ORL-Face",
+              val_batch_size = 400,
+              is_rgb = False,
               dim_embedding = 256,
+              early_stopping_patience = 15,
+              reduce_lr_on_plateau = dict(patience= 5, factor=0.2),
               )
+
+# # test
+# config = dict(train_set_root = "e:/src/jupyter/datasets/ORL-Face",
+#               train_batch_size = 64,
+#               train_epochs = 100,
+#               val_set_root = "e:/src/jupyter/datasets/ORL-Face",
+#               val_batch_size = 400,
+#               is_rgb = False,
+#               dim_embedding = 256,
+#               early_stopping_patience = 15,
+#               reduce_lr_on_plateau = dict(patience= 5, factor=0.2),
+#               )
 
 if __name__ == "__main__":
 
--- utils/utils.py
+++ utils/utils.py
@@ -20,3 +20,20 @@ def imshow(torch_pil_img, text=None, is_torch=False):
 def plot_loss(iteration, loss):
     plt.plot(iteration, loss)
     plt.show()
+
+def plot_history(history, filename=None):
+    
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_subplot(121)
+    ax.plot(history["epoch"], history["train_loss"], label="train loss")
+    ax.plot(history["epoch"], history["val_loss"], label="val loss")
+    ax = fig.add_subplot(122)
+    ax.semilogy(history["epoch"], history["train_loss"], label="train loss")
+    ax.semilogy(history["epoch"], history["val_loss"], label="val loss")
+    
+    if filename is not None:
+        plt.savefig(filename)
+    
+    plt.show()
+    
+    
